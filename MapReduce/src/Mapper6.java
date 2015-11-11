cc1f092bc88bf81086e6839198fcecacfc9e9353
Danni Wang <dannywang0911@gmail.com>
2015-11-10 20:51:05 -0600

    fix Mapper6

--- MapReduce/src/Mapper6.java
+++ MapReduce/src/Mapper6.java
@@ -7,18 +7,26 @@ import org.apache.hadoop.mapreduce.Mapper.Context;
 // input text format: 
 // targetId sourceId distance weight status pathList adjList
 
-public class Mapper6 extends Mapper<Object, Text, Text, Text> {
+public class Mapper6 extends Mapper<Object, Text, Text, Text> {	
 	public void map(Object key, Text value, Context context) {
 		String[] str = value.toString().split(" |\\t");
 		
 		String targetId = str[0];
 		String[] adj = str[6].substring(1, str[6].length() - 1).split(",");
 		
-		for (String user: adj) {
+		if (adj.length == 1) {
 			try {
-				context.write(new Text(targetId + "," + user), new Text());
+				context.write(new Text(targetId + "," + targetId), new Text());
 			} catch (Exception e) {
 				e.printStackTrace();
+			}
+		} else {
+			for (String user: adj) {
+				try {
+					context.write(new Text(targetId + "," + user), new Text());
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
 			}
 		}
 	}
